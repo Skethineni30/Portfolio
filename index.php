f38b1ef13b793c32f54489ac4654a7b240cdc551
Skethineni30 <57237535+Skethineni30@users.noreply.github.com>
2020-04-20 10:57:25 -0700

    Added the files from Replit Workload

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Hello World! Here are all of my links, Surya Kethineni Period 6</p>'?> 
+    <ul>
+      <li><a href="index.html">index.html</a></li></br>
+      <li><a href="Kethineni_AboutMe.html">About Me</a></li></br>
+      <li><a href="Kethineni_Projects.html">Projects</a></li></br>
+      <li><a href="Kethineni_Resume.html">Resume</a></li></br>
+      <li><a href="Kethineni_ContactMe.html">Contact Me</a></li></br>
+    <ul>
+  </body>
+</html>
